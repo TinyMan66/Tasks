3759f9ec6c4dbd746290070ede5b27d104ec8d5e
диана липанова <dianalipanova@MacBook-Pro-diana.local>
2021-04-21 21:30:02 +0300

    Add new operators

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,4 +1,17 @@
-user_input = input('Что вычислить?')
-x, y = user_input.split("+")
-i = int(x) + int(y)
-print(i)
+
+proceed = "y"
+while proceed == "y":
+    f_num = float(input("Enter the first number>>"))
+    oper = input("Enter operation>>")
+    s_num = float(input("Enter the second number>>"))
+    if oper == "+":
+        print(f_num + s_num)
+    elif oper == "-":
+        print(f_num - s_num)
+    elif oper == "*":
+        print(f_num * s_num)
+    elif oper == "/":
+        print(f_num % s_num)
+    else:
+        print("Error")
+    proceed = input("Enter 'y' to continue and other key to finish>>")
