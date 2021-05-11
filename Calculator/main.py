d5fa4c4899b5462c63dd2e39185f94449ab050f0
@DiamaL-66 <diana.696@mail.ru>
2021-05-11 16:01:58 +0300

    Added dictionary with functions

--- Calculator/main.py
+++ Calculator/main.py
@@ -1,17 +1,28 @@
 
-proceed = "y"
-while proceed == "y":
-    f_num = float(input("Enter the first number>>"))
-    oper = input("Enter operation>>")
-    s_num = float(input("Enter the second number>>"))
-    if oper == "+":
-        print(f_num + s_num)
-    elif oper == "-":
-        print(f_num - s_num)
-    elif oper == "*":
-        print(f_num * s_num)
-    elif oper == "/":
-        print(f_num % s_num)
-    else:
-        print("Error")
-    proceed = input("Enter 'y' to continue and other key to finish>>")
+def main():
+  operations_dict = {
+    "+": lambda f_num, s_num: f_num + s_num,
+    "-": lambda f_num, s_num: f_num - s_num,
+    "*": lambda f_num, s_num: f_num * s_num,
+    "/": lambda f_num, s_num: f_num / s_num
+  }
+
+  f_num = float(input("Enter the first number>>"))
+  oper = input("Enter operation>>")
+  s_num = float(input("Enter the second number>>"))
+  if oper == "+":
+      print(operations_dict["+"](f_num, s_num))
+  elif oper == "-":
+      print(operations_dict["-"](f_num, s_num))
+  elif oper == "*":
+      print(operations_dict["*"](f_num, s_num))
+  elif oper == "/":
+      print(operations_dict["/"](f_num, s_num))
+  else:
+      print("Error")
+main()
+
+
+
+
+
